f0df00d1a66de5e8d3ee274c57fd1e06ce07e9ba
Selina Chua <selina.a.chua@gmail.com>
2020-06-13 00:48:37 +1000

    Add an on click function to the add button.

--- app/src/main/java/com/connect/ui/calendar/CalendarFragment.java
+++ app/src/main/java/com/connect/ui/calendar/CalendarFragment.java
@@ -10,16 +10,27 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.connect.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class CalendarFragment extends Fragment {
 
     private CalendarViewModel calendarViewModel;
+    private FloatingActionButton addButton;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         calendarViewModel =
                 ViewModelProviders.of(this).get(CalendarViewModel.class);
         View root = inflater.inflate(R.layout.fragment_calendar, container, false);
+
+        addButton = (FloatingActionButton) root.findViewById(R.id.addButton);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Future Feature: add event to calendar.
+            }
+        });
+
         return root;
     }
 }
