112ed98b4585e2b615ad50f9724aebb939278538
Selina Chua <selina.a.chua@gmail.com>
2020-06-12 16:32:13 +1000

    Added sliding functionality to main three pages.

--- app/src/main/java/com/connect/MainActivity.java
+++ app/src/main/java/com/connect/MainActivity.java
@@ -2,29 +2,35 @@ package com.connect;
 
 import android.os.Bundle;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+import androidx.viewpager.widget.ViewPager;
+
+import com.connect.ui.SlideAdapter;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewPager slideViewPager;
+    private SlideAdapter slideAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
+//        BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);
+//        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+//                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+//                .build();
+//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+//        NavigationUI.setupWithNavController(navView, navController);
+
+        slideViewPager = (ViewPager) findViewById(R.id.slideViewPager);
+        slideAdapter = new SlideAdapter(this);
+        slideViewPager.setAdapter(slideAdapter);
+
     }
 
 }
