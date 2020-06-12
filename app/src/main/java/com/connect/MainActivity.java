a0de82345e3a38ba3619329d4b83b8cc78ae2b52
Selina Chua <selina.a.chua@gmail.com>
2020-06-12 19:18:56 +1000

    Current page is highlighted in the nav bar.

--- app/src/main/java/com/connect/MainActivity.java
+++ app/src/main/java/com/connect/MainActivity.java
@@ -5,6 +5,7 @@ import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.ui.AppBarConfiguration;
 import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -14,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     private ViewPager slideViewPager;
     private SlideAdapter slideAdapter;
     private BottomNavigationView bottomNavView;
+    private AppBarConfiguration appBarConfiguration;
 
 
     @Override
@@ -36,6 +38,13 @@ public class MainActivity extends AppCompatActivity {
 
         // Bottom Navigation View
         bottomNavView = (BottomNavigationView) findViewById(R.id.bottomNavView);
+//        appBarConfiguration = new AppBarConfiguration.Builder(
+//                R.id.navigation_profile, R.id.navigation_home, R.id.navigation_calendar
+//        ).build();
+//        NavController navController = Navigation.findNavController(this, R.id.slideViewPager);
+////        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+//        NavigationUI.setupWithNavController(bottomNavView, navController);
+
         bottomNavView.setOnNavigationItemSelectedListener(
                 new BottomNavigationView.OnNavigationItemSelectedListener() {
                     @Override
@@ -44,12 +53,15 @@ public class MainActivity extends AppCompatActivity {
                             // add all the possible navigations.
                             case R.id.navigation_profile:
                                 slideViewPager.setCurrentItem(0);
+                                item.setChecked(true);
                                 break;
                             case R.id.navigation_home:
                                 slideViewPager.setCurrentItem(1);
+                                item.setChecked(true);
                                 break;
                             case R.id.navigation_calendar:
                                 slideViewPager.setCurrentItem(2);
+                                item.setChecked(true);
                                 break;
                         }
                         return false;
